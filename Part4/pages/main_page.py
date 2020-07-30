7f97cf80793cf3b28d22bc48b3b0b5f7c1ed53a1
unknown <C:\Users\Kreedy\AppData\Roaming\The Bat!>
2020-07-30 21:01:47 +0700

    Editing the BasePage constructor and implementing the exception method, adding assert.

--- Part4/pages/main_page.py
+++ Part4/pages/main_page.py
@@ -5,4 +5,8 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
-
+    
+    def should_be_login_link(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        assert self.browser.find_element(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        login_link.click()
