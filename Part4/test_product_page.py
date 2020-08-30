e0a551f488e02af4118b088afcc66994d23a7862
unknown <C:\Users\Kreedy\AppData\Roaming\The Bat!>
2020-08-30 17:07:33 +0700

    The code was checked for compliance with the requirements of task 4.3.14, and errors in passing tests were fixed.

--- Part4/test_product_page.py
+++ Part4/test_product_page.py
@@ -1,6 +1,6 @@
 import pytest
 import time
-from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
 
@@ -105,5 +105,4 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.should_be_add_to_basket_button()
     product_page.solve_quiz_and_get_code()
     product_page.message_should_be_disappeared_after_adding_product_to_basket()
-
 
